f424051b8fb929a1f851f085d151bf82b448ce6c
Markus Schwed <schwed@avarteq.de>
2011-11-23 23:52:01 +0100

    Added install.sh basic

--- install.sh
+++ install.sh
@@ -2,3 +2,10 @@
 current_dir=$(pwd)
 script_dir=$(dirname $0)
 
+#for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
+ln -s $script_dir/vim ~/.vim
+ln -s $script_dir/swvimrc ~/.vimrc
+
+echo "Now, you're almost finished. Just follow these following steps and you're ready to go."
+echo "gem install vim-update-bundles"
+echo "vim-update-bundles"
